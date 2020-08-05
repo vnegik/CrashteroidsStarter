ea5f9d7fce31f49340a7b2cd5d59a539cbe6ed07
Vitali Nikalaenka <vnikalaenka@openmygame.com>
2020-08-05 15:01:47 +0300

    Add unstaged files

---
+++ Assets/RW/Tests/GeneralTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class GeneralTest
+    {
+        public Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+    }
+}
