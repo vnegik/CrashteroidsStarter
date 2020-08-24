2c0466f97721a3519142af46f71e38b6bb06932c
Vitali Nikalaenka <vnikalaenka@openmygame.com>
2020-08-24 11:41:13 +0300

    test vector

---
+++ Assets/RW/Tests/TheSecondTestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Tests
+{
+    public class TheSecondTestScript : GeneralTest
+    {
+        [UnityTest]
+        public IEnumerator DestroyedAsteroidRaisesScore()
+        {
+            // 1
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            // 2
+            Assert.AreEqual(game.score, 1);
+        }
+    }
+}
